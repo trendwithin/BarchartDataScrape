26a1ef2ea6da13838ea15ea6471b43b2b104333c
Michael Becco <e4e6d4d5@gmail.com>
2015-06-22 14:55:40 -0700

    Pass Test to Strip Symbols from Test Data

--- lib/mechanize_alltimehigh.rb
+++ lib/mechanize_alltimehigh.rb
@@ -1,3 +1,20 @@
+require 'mechanize'
+require 'fakeweb'
 class MechanizeAllTimeHigh
+  attr_reader :url, :agent
 
+  def initialize
+    @url = "http://www.barchart.com/stocks/athigh.php?_dtp1-0"
+    @agent = Mechanize.new
+  end
+
+  def parse_url
+    page = Object
+    stream_array = Array.new
+
+    # for current testing purposes use FakeWeb
+    stream = "test/athigh.php"
+    FakeWeb.register_uri(:get, @url, :body => stream, :content_type => 'text/html')
+    page = @agent.get(@url).search("input")
+  end
 end
--- test/mechanize_alltimehigh_test.rb
+++ test/mechanize_alltimehigh_test.rb
@@ -2,8 +2,38 @@ require './test/minitest_helper'
 
 class MechanizeAllTimeHighTest < Minitest::Test
 
-  def test_it_instantiates_mechanize_barchart
+  def setup
     @mb = MechanizeAllTimeHigh.new
+  end
+
+  def test_it_instantiates_mechanize_barchart
     assert_instance_of MechanizeAllTimeHigh, @mb
+  end
+
+  def test_url
+    assert_equal "http://www.barchart.com/stocks/athigh.php?_dtp1-0", @mb.url
+  end
+
+  def test_agent
+    assert_instance_of Mechanize, @mb.agent
+  end
+
+  def test_agent_returns_nodeset
+    assert_instance_of Nokogiri::XML::NodeSet, @mb.parse_url
+  end
+
+  def test_agent_is_converted_to_string
+    assert_instance_of String, @mb.parse_url.to_s
+  end
+
+  def test_symbols_include_expected_values
+    agent = @mb.parse_url[6].to_s
+    strip_tickers = agent.scan(/[A-Z]+,[^a-z]+[A-Z]/)
+    symbols = strip_tickers[0].split(',')
+    assert_instance_of Array, symbols
+    expected = ['AAT', 'LOPE', 'ZMH']
+    expected.each do |e|
+      assert_includes symbols, e
+    end
   end
 end
