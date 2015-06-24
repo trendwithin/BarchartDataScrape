3086418e4c02ac387da19376e41e5692553804db
Michael Becco <e4e6d4d5@gmail.com>
2015-06-23 19:24:35 -0700

    Add require for newhigh

--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -2,5 +2,6 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'barchart_data_scrape'
 require 'mechanize_alltimehigh'
 require 'alltimehigh'
+require 'newhigh'
 require 'minitest/autorun'
 require 'active_record'
