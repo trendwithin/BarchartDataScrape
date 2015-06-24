f58819d208b8a8dbaa093b7b56c46c8fd9206c6e
Michael Becco <e4e6d4d5@gmail.com>
2015-06-23 19:04:55 -0700

    Refactor and Add Insert Method

--- lib/alltimehigh.rb
+++ lib/alltimehigh.rb
@@ -1,12 +1,16 @@
 require 'active_record'
-
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database  => "barchart_example"
-)
+require 'active_record_connection'
 
 class AllTimeHigh < ActiveRecord::Base
+  include ActiveRecordConnection
   validates :symbol, :saved_on, presence: true
   # validates_uniqueness_of :symbol, conditions: -> { where.not(saved_on: Time.now.to_date)}
+
+  def insert(page)
+    current_date = Time.now.to_date.to_s
+    page.each do |ticker|
+      AllTimeHigh.create(symbol: ticker, saved_on: current_date)
+    end
+  end
 end
 
